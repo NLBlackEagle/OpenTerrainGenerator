542fbbb1118e2045cdb5272b1623520277b4eb74
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-10-09 02:06:51 +0200

    Clean up BlockPos3D

--- common/src/main/java/com/pg85/otg/util/BlockPos3D.java
+++ common/src/main/java/com/pg85/otg/util/BlockPos3D.java
@@ -2,30 +2,29 @@ package com.pg85.otg.util;
 
 public class BlockPos3D
 {
-	final int x;
-	final int y;
-	final int z;
-	
-	public BlockPos3D(int x, int y, int z)
-	{
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
-	
-	public boolean equals(Object other)
-	{
-		if(this == other)
-		{
-			return true;
-		}
-		if(other instanceof BlockPos3D)
-		{
-			if(((BlockPos3D)other).x == this.x && ((BlockPos3D)other).z == this.z)
-			{
-				return true;
-			}
-		}
-		return false;
-	}
+    public final int x;
+    public final int y;
+    public final int z;
+
+    public BlockPos3D(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(this == obj)
+        {
+            return true;
+        }
+        if(obj instanceof BlockPos3D)
+        {
+            BlockPos3D other = (BlockPos3D) obj;
+            return this.x == other.x && this.z == other.z;
+        }
+        return false;
+    }
 }
