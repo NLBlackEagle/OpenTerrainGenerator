535df24bd520d0f0e51b4ac3a25de82e11a5e2e7
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-10-19 17:49:01 +0200

    Add methods to create (de-)compression streams

--- common/src/main/java/com/pg85/otg/util/CompressionUtils.java
+++ common/src/main/java/com/pg85/otg/util/CompressionUtils.java
@@ -1,10 +1,23 @@
 package com.pg85.otg.util;
 
 import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.Channels;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
 
 import com.pg85.otg.OTG;
 import com.pg85.otg.logging.LogMarker;
@@ -50,4 +63,35 @@ public class CompressionUtils
 		//OTG.log(LogMarker.INFO, "Decompressed: " + output.length);
 		return output;  
 	}
+
+    public static InputStream newInflaterInputStream(Path file) throws IOException
+    {
+        return new InflaterInputStream(Files.newInputStream(file));
+    }
+
+    public static OutputStream newDeflaterOutputStream(Path file) throws IOException
+    {
+        return new DeflaterOutputStream(Files.newOutputStream(file));
+    }
+
+    public static InputStream newGZIPInputStream(Path file) throws IOException
+    {
+        SeekableByteChannel channel = Files.newByteChannel(file);
+        ByteBuffer buf = ByteBuffer.allocate(2).order(ByteOrder.nativeOrder());
+        while(buf.hasRemaining())
+        {
+            if(channel.read(buf) < 0)
+            {
+                throw new EOFException();
+            }
+        }
+        channel.position(0);
+        InputStream in = Channels.newInputStream(channel);
+        return (buf.getShort(0) & 0xFFFF) == GZIPInputStream.GZIP_MAGIC ? new GZIPInputStream(in) : in;
+    }
+
+    public static OutputStream newGZIPOutputStream(Path file) throws IOException
+    {
+        return new GZIPOutputStream(Files.newOutputStream(file));
+    }
 }
