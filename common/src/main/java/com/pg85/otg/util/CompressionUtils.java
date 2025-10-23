a2b1edcbb8e0a94450e58c853a33df304b3f5aa7
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-10-23 18:05:40 +0200

    Remove unused (de-)compress methods

--- common/src/main/java/com/pg85/otg/util/CompressionUtils.java
+++ common/src/main/java/com/pg85/otg/util/CompressionUtils.java
@@ -1,6 +1,5 @@
 package com.pg85.otg.util;
 
-import java.io.ByteArrayOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,59 +10,13 @@ import java.nio.channels.Channels;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.zip.DataFormatException;
-import java.util.zip.Deflater;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
-import java.util.zip.Inflater;
 import java.util.zip.InflaterInputStream;
-
-import com.pg85.otg.OTG;
-import com.pg85.otg.logging.LogMarker;
 
 public class CompressionUtils
 {
-	public static byte[] compress(byte[] data) throws IOException
-	{  
-		Deflater deflater = new Deflater();  
-		deflater.setInput(data);  
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);   
-		deflater.finish();  
-		byte[] buffer = new byte[1024];   
-		while (!deflater.finished())
-		{  
-			int count = deflater.deflate(buffer); // returns the generated code... index  
-			outputStream.write(buffer, 0, count);   
-		}  
-		outputStream.close();  
-		byte[] output = outputStream.toByteArray();
-		if(OTG.getPluginConfig().spawnLog)
-		{
-			OTG.log(LogMarker.INFO, "Original: " + data.length / 1024 + " Kb");  
-			OTG.log(LogMarker.INFO, "Compressed: " + output.length / 1024 + " Kb");
-		}
-		return output;  
-	}
-
-	public static byte[] decompress(byte[] data) throws IOException, DataFormatException
-	{  
-		Inflater inflater = new Inflater();   
-		inflater.setInput(data);  
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);  
-		byte[] buffer = new byte[1024];  
-		while (!inflater.finished())
-		{  
-			int count = inflater.inflate(buffer);  
-			outputStream.write(buffer, 0, count);  
-		}  
-		outputStream.close();  
-		byte[] output = outputStream.toByteArray();
-		//OTG.log(LogMarker.INFO, "Original: " + data.length);  
-		//OTG.log(LogMarker.INFO, "Decompressed: " + output.length);
-		return output;  
-	}
-
     public static InputStream newInflaterInputStream(Path file) throws IOException
     {
         return new InflaterInputStream(Files.newInputStream(file));
