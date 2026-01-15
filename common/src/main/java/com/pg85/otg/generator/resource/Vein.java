3a2bd36da0968d9bd9686e558995ec726bf0261c
Meldexun <27779321+Meldexun@users.noreply.github.com>
2026-01-15 23:45:08 +0100

    Rename Vein.spawnOre variables

--- common/src/main/java/com/pg85/otg/generator/resource/Vein.java
+++ common/src/main/java/com/pg85/otg/generator/resource/Vein.java
@@ -74,49 +74,49 @@ class Vein
 
         float f = rand.nextFloat() * 3.141593F;
 
-        double d1 = x + 8 + MathHelper.sin(f) * maxSize / 8.0F;
-        double d2 = x + 8 - MathHelper.sin(f) * maxSize / 8.0F;
-        double d3 = z + 8 + MathHelper.cos(f) * maxSize / 8.0F;
-        double d4 = z + 8 - MathHelper.cos(f) * maxSize / 8.0F;
+        double maxX = x + 8 + MathHelper.sin(f) * maxSize / 8.0F;
+        double minX = x + 8 - MathHelper.sin(f) * maxSize / 8.0F;
+        double maxZ = z + 8 + MathHelper.cos(f) * maxSize / 8.0F;
+        double minZ = z + 8 - MathHelper.cos(f) * maxSize / 8.0F;
 
-        double d5 = y + rand.nextInt(3) - 2;
-        double d6 = y + rand.nextInt(3) - 2;
+        double maxY = y + rand.nextInt(3) - 2;
+        double minY = y + rand.nextInt(3) - 2;
 
         for (int i = 0; i < maxSize; i++)
         {
             float iFactor = (float) i / (float) maxSize;
-            double d7 = d1 + (d2 - d1) * iFactor;
-            double d8 = d5 + (d6 - d5) * iFactor;
-            double d9 = d3 + (d4 - d3) * iFactor;
+            double x1 = maxX + (minX - maxX) * iFactor;
+            double y1 = maxY + (minY - maxY) * iFactor;
+            double z1 = maxZ + (minZ - maxZ) * iFactor;
 
             double d10 = rand.nextDouble() * maxSize / 16.0D;
             double d11 = (MathHelper.sin((float) Math.PI * iFactor) + 1.0) * d10 + 1.0;
             double d12 = (MathHelper.sin((float) Math.PI * iFactor) + 1.0) * d10 + 1.0;
 
-            int j = MathHelper.floor(d7 - d11 / 2.0D);
-            int k = MathHelper.floor(d8 - d12 / 2.0D);
-            int m = MathHelper.floor(d9 - d11 / 2.0D);
+            int minX1 = MathHelper.floor(x1 - d11 / 2.0D);
+            int minY1 = MathHelper.floor(y1 - d12 / 2.0D);
+            int minZ1 = MathHelper.floor(z1 - d11 / 2.0D);
 
-            int n = MathHelper.floor(d7 + d11 / 2.0D);
-            int i1 = MathHelper.floor(d8 + d12 / 2.0D);
-            int i2 = MathHelper.floor(d9 + d11 / 2.0D);
+            int maxX1 = MathHelper.floor(x1 + d11 / 2.0D);
+            int maxY1 = MathHelper.floor(y1 + d12 / 2.0D);
+            int maxZ1 = MathHelper.floor(z1 + d11 / 2.0D);
 
-            for (int i3 = j; i3 <= n; i3++)
+            for (int x2 = minX1; x2 <= maxX1; x2++)
             {
-                double d13 = (i3 + 0.5D - d7) / (d11 / 2.0D);
-                if (d13 * d13 < 1.0D)
+                double dx = (x2 + 0.5D - x1) / (d11 / 2.0D);
+                if (dx * dx < 1.0D)
                 {
-                    for (int i4 = k; i4 <= i1; i4++)
+                    for (int y2 = minY1; y2 <= maxY1; y2++)
                     {
-                        double d14 = (i4 + 0.5D - d8) / (d12 / 2.0D);
-                        if (d13 * d13 + d14 * d14 < 1.0D)
+                        double dy = (y2 + 0.5D - y1) / (d12 / 2.0D);
+                        if (dx * dx + dy * dy < 1.0D)
                         {
-                            for (int i5 = m; i5 <= i2; i5++)
+                            for (int z2 = minZ1; z2 <= maxZ1; z2++)
                             {
-                                double d15 = (i5 + 0.5D - d9) / (d11 / 2.0D);
-                                if ((d13 * d13 + d14 * d14 + d15 * d15 < 1.0D) && sourceBlocks.contains(world.getMaterial(i3, i4, i5, chunkBeingPopulated)))
+                                double dz = (z2 + 0.5D - z1) / (d11 / 2.0D);
+                                if ((dx * dx + dy * dy + dz * dz < 1.0D) && sourceBlocks.contains(world.getMaterial(x2, y2, z2, chunkBeingPopulated)))
                                 {
-                                    world.setBlock(i3, i4, i5, material, null, chunkBeingPopulated, true);
+                                    world.setBlock(x2, y2, z2, material, null, chunkBeingPopulated, true);
                                 }
                             }
                         }
